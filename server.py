ee8848f940b68a395edf8e8f0fa3f37c3a428f47
Vicente Neto <sneto.vicente@gmail.com>
2017-01-11 22:48:26 -0300

    Adding paramiko to send and execute client script

--- server.py
+++ server.py
@@ -14,3 +14,75 @@ design the database table yourself.
 the client configured email address using SMTP. Use a simple text mail format with some detail about the alert. event
 logs must be sent via email every time without any condition.
 """
+
+from xml.etree import ElementTree
+
+from paramiko import SFTPClient, Transport
+
+
+def validate_parameters(element, parameters):
+    return all(parameter in element.attrib for parameter in parameters)
+
+
+tree = ElementTree.parse('config.xml')
+alert_parameters = ('type', 'limit')
+client_parameters = ('ip', 'username', 'password')
+
+
+class Alert(object):
+    def __init__(self, type_, limit):
+        self.type = type_
+        self.limit = limit
+
+    @staticmethod
+    def from_element(element):
+        return Alert(element.get('type'), element.get('limit'))
+
+
+class Client(object):
+    client = None
+
+    def __init__(self, ip, port, username, password, mail, alerts):
+        self.ip = ip
+        self.port = port
+        self.username = username
+        self.password = password
+        self.mail = mail
+        self.alerts = [Alert.from_element(alert_element) for alert_element in alerts if
+                       validate_parameters(alert_element, alert_parameters)]
+
+    @staticmethod
+    def from_element(element):
+        return Client(element.get('ip'), int(element.get('port', 22)), element.get('username'), element.get('password'),
+                      element.get('mail', None), list(element))
+
+    def connect(self):
+        self.client = Transport((self.ip, self.port))
+        self.client.connect(username=self.username, password=self.password)
+
+    def close(self):
+        self.client.close()
+
+    def send_client_script(self):
+        sftp = SFTPClient.from_transport(self.client)
+        sftp.put('client.py', 'client.py')
+
+    def execute_client_script(self):
+        session = self.client.open_channel(kind='session')
+        session.exec_command('python client.py')
+
+        while True:
+            if session.exit_status_ready():
+                break
+
+        session.close()
+
+
+for client_element in tree.getroot():
+    if validate_parameters(client_element, client_parameters):
+        client = Client.from_element(client_element)
+
+        client.connect()
+        client.send_client_script()
+        client.execute_client_script()
+        client.close()
