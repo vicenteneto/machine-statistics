66344c48f924984421a45e8d72b9107b14aed6b0
Vicente Neto <sneto.vicente@gmail.com>
2017-01-13 01:05:28 -0300

    Collecting statistics and returning to the server

--- client.py
+++ client.py
@@ -6,3 +6,69 @@ case of windows OS).
 2. When executed, the client script collects the statistics and return them to the server script for cumulation.
 3. The client script must encrypt the data before returning it to server.
 """
+import pickle
+from datetime import datetime
+
+import psutil
+from Crypto import Random
+from Crypto.Cipher import AES
+
+
+class File(object):
+    KEY = b'\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18'
+
+    def __init__(self, name, mode):
+        if name.endswith('.enc'):
+            raise ValueError('name cannot ends with ".enc"')
+        self.file = open(name, mode)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+
+    @staticmethod
+    def pad(s):
+        return s + b'\0' * (AES.block_size - len(s) % AES.block_size)
+
+    def __encrypt_text(self, message):
+        message = self.pad(message)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.KEY, AES.MODE_CBC, iv)
+        return iv + cipher.encrypt(message)
+
+    def encrypt(self):
+        plaintext = self.file.read()
+        cipher_text = self.__encrypt_text(plaintext)
+        with open(self.file.name + '.enc', 'wb') as enc_file:
+            enc_file.write(cipher_text)
+
+
+execution_date = datetime.utcnow()
+cpu_percent = psutil.cpu_percent(interval=1)
+virtual_memory = psutil.virtual_memory()
+disk_partitions = psutil.disk_partitions()
+uptime = psutil.boot_time()
+
+disk_usage = dict()
+for partition in disk_partitions:
+    usage = psutil.disk_usage(partition.mountpoint)
+    disk_usage[partition.mountpoint] = {'used': usage.used, 'percent': usage.percent}
+
+metrics = {
+    'execution_date': execution_date,
+    'cpu': cpu_percent,
+    'memory': {
+        'used': virtual_memory.used,
+        'percent': virtual_memory.percent
+    },
+    'disk': disk_usage,
+    'uptime': uptime
+}
+
+with open('/tmp/client_data.json', 'wb') as data_file:
+    pickle.dump(metrics, data_file)
+
+with File('/tmp/client_data.json', 'rb') as data_file:
+    data_file.encrypt()
--- server.py
+++ server.py
@@ -14,19 +14,49 @@ design the database table yourself.
 the client configured email address using SMTP. Use a simple text mail format with some detail about the alert. event
 logs must be sent via email every time without any condition.
 """
-
+import pickle
 from xml.etree import ElementTree
 
+from Crypto.Cipher import AES
 from paramiko import SFTPClient, Transport
+from sqlalchemy.engine import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.schema import Column
+from sqlalchemy.types import DateTime, Integer, String
+
+ALERT_PARAMETERS = ('type', 'limit')
+CLIENT_PARAMETERS = ('ip', 'username', 'password')
+CONFIG_FILE = 'config/config.xml'
+
+engine = create_engine('mysql://root:6esncj4m@localhost/metrics')
+Base = declarative_base()
 
 
-def validate_parameters(element, parameters):
-    return all(parameter in element.attrib for parameter in parameters)
+class File(object):
+    KEY = b'\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18'
 
+    def __init__(self, filename):
+        if not filename.endswith('.enc'):
+            raise ValueError('Filename must ends with ".enc"')
+        self.file = open(filename)
 
-tree = ElementTree.parse('config.xml')
-alert_parameters = ('type', 'limit')
-client_parameters = ('ip', 'username', 'password')
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+
+    def __decrypt_text(self, cipher_text):
+        iv = cipher_text[:AES.block_size]
+        cipher = AES.new(self.KEY, AES.MODE_CBC, iv)
+        plaintext = cipher.decrypt(cipher_text[AES.block_size:])
+        return plaintext.rstrip(b'\0')
+
+    def decrypt(self):
+        cipher_text = self.file.read()
+        plaintext = self.__decrypt_text(cipher_text)
+        with open(self.file.name[:-4], 'wb') as txt_file:
+            txt_file.write(plaintext)
 
 
 class Alert(object):
@@ -39,8 +69,32 @@ class Alert(object):
         return Alert(element.get('type'), element.get('limit'))
 
 
-class Client(object):
+class BatchCommands(object):
+    def __init__(self, session, command):
+        self.session = session
+        self.command = command
+
+    def add_command(self, command):
+        self.command += ' && ' + command
+
+    def exec_commands(self):
+        self.session.exec_command(self.command)
+
+
+class Metric(Base):
+    __tablename__ = 'metrics'
+
+    id = Column(Integer, primary_key=True)
+    execution_date = Column(DateTime, nullable=False)
+
+
+class Client(Base):
+    __tablename__ = 'clients'
+
     client = None
+
+    id = Column(Integer, primary_key=True)
+    ip = Column(String(15), unique=True)
 
     def __init__(self, ip, port, username, password, mail, alerts):
         self.ip = ip
@@ -49,7 +103,7 @@ class Client(object):
         self.password = password
         self.mail = mail
         self.alerts = [Alert.from_element(alert_element) for alert_element in alerts if
-                       validate_parameters(alert_element, alert_parameters)]
+                       all(parameter in alert_element.attrib for parameter in ALERT_PARAMETERS)]
 
     @staticmethod
     def from_element(element):
@@ -66,10 +120,17 @@ class Client(object):
     def send_client_script(self):
         sftp = SFTPClient.from_transport(self.client)
         sftp.put('client.py', '/tmp/client.py')
+        sftp.put('client-requirements.txt', '/tmp/requirements.txt')
 
     def execute_client_script(self):
         session = self.client.open_channel(kind='session')
-        session.exec_command('python /tmp/client.py')
+
+        batch = BatchCommands(session, 'cd /tmp')
+        batch.add_command('virtualenv --python=/usr/local/lib/python2.7.13/bin/python pyEnv')
+        batch.add_command('source pyEnv/bin/activate')
+        batch.add_command('pip install -r requirements.txt')
+        batch.add_command('python client.py')
+        batch.exec_commands()
 
         while True:
             if session.exit_status_ready():
@@ -77,12 +138,36 @@ class Client(object):
 
         session.close()
 
+    def create_client_data_file_name(self):
+        return 'client_data_%s.json' % self.ip
+
+    def save_client_data(self):
+        client_data_filename = self.create_client_data_file_name()
+        sftp = SFTPClient.from_transport(self.client)
+        sftp.get('/tmp/client_data.json.enc', '/tmp/' + client_data_filename + '.enc')
+
+    def decrypt_client_data(self):
+        client_data_filename = self.create_client_data_file_name()
+        with File('/tmp/' + client_data_filename + '.enc') as data_file:
+            data_file.decrypt()
+
+    def print_metrics(self):
+        client_data_filename = self.create_client_data_file_name()
+        with open('/tmp/' + client_data_filename) as data_file:
+            print pickle.load(data_file)
+
+
+Base.metadata.create_all(engine)
+tree = ElementTree.parse(CONFIG_FILE)
 
 for client_element in tree.getroot():
-    if validate_parameters(client_element, client_parameters):
+    if all(parameter in client_element.attrib for parameter in CLIENT_PARAMETERS):
         client = Client.from_element(client_element)
 
         client.connect()
         client.send_client_script()
         client.execute_client_script()
+        client.save_client_data()
+        client.decrypt_client_data()
+        client.print_metrics()
         client.close()
